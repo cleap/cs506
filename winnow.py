69ed1328db2cf5fb5831f44cc643674c114f2632
John Tran <jtran989@unm.edu>
2024-04-18 15:08:53 -0600

    Finished the binary part of winnow (outputs may vary because of randomness...)

--- winnow.py
+++ winnow.py
@@ -4,8 +4,11 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Constants
+PLOT = False
 
-def winnow(points, n, T, eta, rho, epsilon, alpha=0.0):
+
+def winnow(points, n, T, eta, rho, alpha=0.0):
     # Assume M = rho in the notes...where rho is given as 1 (maximum 
     #  value of any entry in any feature vector)
 
@@ -19,7 +22,10 @@ def winnow(points, n, T, eta, rho, epsilon, alpha=0.0):
     u_vec = np.ones((n,))
     w_vec = np.ones((n,))
 
-    t_total = 0
+    # set to T since it will be overridden if it terminates within T
+    t_total = T
+
+    terminate = False
 
     for t in range(T):
         # find a misclassified point
@@ -29,6 +35,7 @@ def winnow(points, n, T, eta, rho, epsilon, alpha=0.0):
 
         if (classification_checks >= alpha).all():
             t_total = t
+            terminate = True
             break
         else:
             # mask the classification checks (1 if less than 0; 0 otherwise)
@@ -62,13 +69,86 @@ def winnow(points, n, T, eta, rho, epsilon, alpha=0.0):
     classification_checks = points @ w_vec
     print(f"classification_checks = {classification_checks}")
 
+    return terminate
+
 
 def winnow_lm(points, n, T, eta, rho, epsilon):
     """
     winnow large margin
     """
 
-    winnow(points, n, T, eta, rho, epsilon, alpha=(epsilon / 2))
+    winnow(points, n, T, eta, rho, alpha=(epsilon / 2))
+    
+
+def winnow_check_terminate(points, n, T, eta, rho, alpha, num_tests):
+    """
+    Since randomness is involved in Winnow, we need to apply some sufficient 
+    number of runs to make sure some termination 
+    """
+    
+    terminate = False
+    for i in range(num_tests):
+        terminate = terminate or winnow(points, n, T, eta, rho, alpha=alpha)
+        
+        # Need to find some terminating solution so we can find the highest 
+        # epsilon
+        if terminate:
+            break
+        
+    return terminate
+
+
+def winnow_binary_increase(points, n, T, eta, rho, epsilon_test, tol, 
+                           num_tests):
+    print()
+    print(f"winnow binary increase: {epsilon_test}")
+    
+    # terminate = winnow(points, n, T, eta, rho, alpha=epsilon_test)
+    terminate = winnow_check_terminate(points, n, T, eta, rho, epsilon_test, 
+                                       num_tests)
+
+    if terminate:
+        winnow_binary_increase(points, n, T, eta, rho, 2 * epsilon_test, tol,
+                               num_tests)
+    else:
+        left_epsilon = epsilon_test / 2
+        right_epsilon = epsilon_test
+        winnow_binary_search(points, n, T, eta, rho, left_epsilon,
+                             right_epsilon, tol, num_tests)
+
+
+def winnow_binary_search(points, n, T, eta, rho, left_epsilon,
+                         right_epsilon, tol, num_tests):
+    middle_epsilon = (left_epsilon + right_epsilon) / 2
+
+    print()
+    print(f"winnow binary search")
+    print(f"left epsilon: {left_epsilon}")
+    print(f"right epsilon: {right_epsilon}")
+    print(f"middle epsilon: {middle_epsilon}")
+
+    if (middle_epsilon - left_epsilon) < tol:
+        # default to left epsilon
+        print()
+        print(f"TOLERANCE REACHED")
+        print(f"final epsilon: {left_epsilon}")
+        return left_epsilon
+    else:
+        # terminate = winnow(points, n, T, eta, rho, alpha=middle_epsilon)
+        terminate = winnow_check_terminate(points, n, T, eta, rho,
+                                           middle_epsilon, num_tests)
+        
+        if terminate:
+            winnow_binary_search(points, n, T, eta, rho, middle_epsilon,
+                                 right_epsilon, tol, num_tests)
+        else:
+            winnow_binary_search(points, n, T, eta, rho, left_epsilon,
+                                 middle_epsilon, tol, num_tests)
+
+
+def winnow_binary(points, n, T, eta, rho, initial_epsilon, tol, num_tests):
+    winnow_binary_increase(points, n, T, eta, rho, initial_epsilon, tol,
+                           num_tests)
 
 
 def main():
@@ -108,52 +188,64 @@ def main():
 
     print()
     print("Normal Winnow:")
-    winnow(new_points, n, T, eta, rho, epsilon)
+    winnow(new_points, n, T, eta, rho)
 
     print()
     print(f"Winnow with margin epsilon/2={epsilon / 2}:")
     winnow_lm(new_points, n, T, eta, rho, epsilon)
 
-    fig, ax = plt.subplots()
-    xs = points[:, 0]
-    ys = points[:, 1]
-    marks = points[:, 2]
-    
-    none_color = "none"
-    black_color = "black"
-    
-    points_dict = dict()
-    points_dict[none_color] = {}
-    points_dict[black_color] = {}
-    points_dict[none_color]["points"] = {}
-    points_dict[black_color]["points"] = {}
-    points_dict[none_color]["points"]["x"] = \
-        [x for (x, mark) in zip(xs, marks) if mark < 0]
-    points_dict[none_color]["points"]["y"] = \
-        [y for (y, mark) in zip(ys, marks) if mark < 0]
-    points_dict[black_color]["points"]["x"] = \
-        [x for (x, mark) in zip(xs, marks) if mark > 0]
-    points_dict[black_color]["points"]["y"] = \
-        [y for (y, mark) in zip(ys, marks) if mark > 0]
-    points_dict[none_color]["label"] = \
-        "class=-1"
-    points_dict[black_color]["label"] = \
-        "class=1"
+    initial_epsilon_binary = epsilon / 2
+    tol = 1e-4
+    num_tests = 30
+    print()
+    print(f"Winnow binary with initial guess=epsilon/2={epsilon / 2}:")
+    print(f"initial epsilon={initial_epsilon_binary}")
+    print(f"tolerance={tol}")
+    print(f"num tests for termination={num_tests}")
+    winnow_binary(new_points, n, T, eta, rho, initial_epsilon_binary, tol,
+                  num_tests)
 
-    for color in [none_color, black_color]:
-        ax.scatter(points_dict[color]["points"]["x"],
-                   points_dict[color]["points"]["y"], 
-                   s=80, 
-                   facecolors=color, 
-                   edgecolors="black",
-                   label=points_dict[color]["label"])
+    if PLOT:
+        fig, ax = plt.subplots()
+        xs = points[:, 0]
+        ys = points[:, 1]
+        marks = points[:, 2]
 
-    plt.xlabel("X")
-    plt.ylabel("Y")
-    plt.title("Plot of Points")
-    plt.legend()
+        none_color = "none"
+        black_color = "black"
 
-    plt.show()
+        points_dict = dict()
+        points_dict[none_color] = {}
+        points_dict[black_color] = {}
+        points_dict[none_color]["points"] = {}
+        points_dict[black_color]["points"] = {}
+        points_dict[none_color]["points"]["x"] = \
+            [x for (x, mark) in zip(xs, marks) if mark < 0]
+        points_dict[none_color]["points"]["y"] = \
+            [y for (y, mark) in zip(ys, marks) if mark < 0]
+        points_dict[black_color]["points"]["x"] = \
+            [x for (x, mark) in zip(xs, marks) if mark > 0]
+        points_dict[black_color]["points"]["y"] = \
+            [y for (y, mark) in zip(ys, marks) if mark > 0]
+        points_dict[none_color]["label"] = \
+            "class=-1"
+        points_dict[black_color]["label"] = \
+            "class=1"
+
+        for color in [none_color, black_color]:
+            ax.scatter(points_dict[color]["points"]["x"],
+                       points_dict[color]["points"]["y"],
+                       s=80,
+                       facecolors=color,
+                       edgecolors="black",
+                       label=points_dict[color]["label"])
+
+        plt.xlabel("X")
+        plt.ylabel("Y")
+        plt.title("Plot of Points")
+        plt.legend()
+
+        plt.show()
 
 
 if __name__ == "__main__":
